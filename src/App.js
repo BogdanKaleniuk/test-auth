78286580d2913fa5c0f712e6082aeb5000515f3d
BogdanKaleniuk <bogdankaleniuk95@gmail.com>
2023-12-04 22:02:25 +0200

    C:/Program Files/Git/3/

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import "./App.css";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginForm from "./Components/LoginForm/LoginForm";
 import Main from "./Components/Main/Main";
 import { Container } from "./Styled.App";
 
 function App() {
   return (
-    // <Router>
-    <Container>
-      <Routes>
-        <Route path="/login" element={<LoginForm />} />
-        <Route exact path="/" element={<Main />} />
-      </Routes>
-    </Container>
-    // </Router>
+    <Router>
+      <Container>
+        <Routes>
+          <Route path="/login" element={<LoginForm />} />
+          <Route exact path="/" element={<Main />} />
+        </Routes>
+      </Container>
+    </Router>
   );
 }
 
